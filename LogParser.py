f53991864af92995db5029dce712c82d93a14429
Leon <leonsin112358@gmail.com>
2014-10-09 17:45:21 +0800

    one transaction parse demo

--- LogParser.py
+++ LogParser.py
@@ -1,34 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys
-from PyQt4 import Qt
+with open('C:\Users\lenovo\Desktop\logFile\POS_FEP_2014-09-26.log.1', 'rb') as f:
+    serialNo = 'fIcjMlq1yN9e'
+    for line in f.readlines():
+        if serialNo in line:
+            print line
 
-class HelloApplication(Qt.QApplication):
-
-    def __init__(self, args):
-        """ In the constructor we're doing everything to get our application
-            started, which is basically constructing a basic QApplication by
-            its __init__ method, then adding our widgets and finally starting
-            the exec_loop."""
-        Qt.QApplication.__init__(self, args)
-        self.addWidgets()
-        self.exec_()
-
-    def addWidgets(self):
-        """ In this method, we're adding widgets and connecting signals from
-            these widgets to methods of our class, the so-called "slots"
-        """
-        self.hellobutton = Qt.QPushButton("Say 'Hello world!'",None)
-        self.connect(self.hellobutton, Qt.SIGNAL("clicked()"), self.slotSayHello)
-        self.hellobutton.show()
-
-    def slotSayHello(self):
-        """ This is an example slot, a method that gets called when a signal is
-            emitted """
-        print ("Hello, World!")
-
-# Only actually do something if this script is run standalone, so we can test our
-# application, but we're also able to import this program without actually running
-# any code.
-if __name__ == "__main__":
-    app = HelloApplication(sys.argv)
